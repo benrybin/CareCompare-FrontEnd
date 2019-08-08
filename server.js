f1a9d4ee5c70fe3fb989374a4aab0f89e9300fbd
benrybinski <benrybin@gmail.com>
2019-08-08 18:26:01 -0400

    t

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/care-compare-front-end'));
+app.use(express.static(__dirname + '/dist/CareCompare-FrontEnd'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/care-compare-front-end/index.html'));
+res.sendFile(path.join(__dirname+'/dist/CareCompare-FrontEnd/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
